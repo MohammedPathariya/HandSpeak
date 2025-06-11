721e9f3978bd11cd77c1ef63656966a7f84cea53
MohammedPathariya <pathmohd123@gmail.com>
2025-06-11 12:05:18 -0400

    Added a json file to deal with the URL problem

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 const videoElement = document.getElementById("video");
 const predictionText = document.getElementById("prediction");
-const BACKEND_URL = "https://huggingface.co/spaces/pathariyamohammed/handspeak-backend";
+const BACKEND_URL = "/predict";
 
 // === Setup MediaPipe Hands ===
 const hands = new Hands({
